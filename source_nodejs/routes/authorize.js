6a98aae2af4cb11173645088ab4d437ccb1a8a92
Nam Nguyễn <nhquynam@users.noreply.github.com>
2017-08-03 11:30:04 +0700

    - update nodejs source

--- source_nodejs/routes/authorize.js
+++ source_nodejs/routes/authorize.js
@@ -1,5 +1,7 @@
 var express = require('express');
+
 var strava = require('strava-v3');
+var uprace = require('../logic/uprace');
 
 var router = express.Router();
 
@@ -14,21 +16,44 @@ router.get('/', function(req, res, next) {
 router.get('/token_exchange', function(req, res, next) {
 
     if (req.query.error){
-
-        var error = {
-            title: "Lỗi đăng ký",
-            message: "Có lỗi xảy ra trong quá trình đăng ký. Xin vui lòng thử lại sau."
-        };
-
-        res.render('error_authorize', { title: 'VNG Level Up 12+1 - UPRACE', error: error });
+        res.redirect('/authorize/error');
     }
     else {
 
         strava.oauth.getToken(req.query.code,function(err, payload, limits) {
-            console.log(payload.access_token);
-            res.render('index', { title: 'VNG Level Up 12+1 - UPRACE', data: [] });
+
+            if (err){
+                res.redirect('/authorize/error');
+            }
+            else {
+
+                console.log(payload.access_token);
+                res.redirect('/authorize/success');
+            }
         });
     }
+});
+
+/* GET authorize success. */
+router.get('/success', function(req, res, next) {
+
+    var result = {
+        title: "Đăng ký thành công",
+        message: "Xin chúc mừng! Bạn đã đăng ký thành công."
+    };
+
+    res.render('authorize', { title: 'VNG Level Up 12+1 - UPRACE', result: result });
+});
+
+/* GET authorize failed. */
+router.get('/error', function(req, res, next) {
+
+    var error = {
+        title: "Lỗi đăng ký",
+        message: "Có lỗi xảy ra trong quá trình đăng ký. Xin vui lòng thử lại sau."
+    };
+
+    res.render('authorize', { title: 'VNG Level Up 12+1 - UPRACE', error: error });
 });
 
 module.exports = router;
--- source_nodejs/routes/index.js
+++ source_nodejs/routes/index.js
@@ -1,116 +1,323 @@
 var express = require('express');
 
-var format = require('format-number');
-var strava = require('strava-v3');
-var moment = require('moment');
+var fs = require('fs');
+var util = require('util');
+var async = require('async');
 
-var uprace = require('../logic/uprace');
+var moment = require('moment');
+var format = require('format-number');
+
+var strava = require('strava-v3');
 
 var router = express.Router();
-var teams = uprace.teams;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    uprace.waitCount = uprace.teams.length;
+    fs.readFile("data/total_ranking.json", function(err, data){
+        
+        if (err){
+            res.render('index', { title: 'VNG Level Up 12+1 - UPRACE', data: [] });
+        }
+        else{
 
-    for(var i=0;i<teams.length;i++){
+            var persons = JSON.parse(data);
 
-        //console.log("- fetching info of team #" + teams[i].id);
-        strava.clubs.get({ id: teams[i].id }, function(err, club, limits){
-
-            if(!err)
+            for(var i=0;i<persons.length;i++)
             {
-                //console.log("- fetching info of team #" + this.team.id + " SUCCESS");
-                uprace.updateTeam(this.team, club);
+                if (persons[i].isspy)
+                    continue;
+
+                persons[i].rank = i+1;
+                persons[i].isTop = i+1 <= 3;
+
+                persons[i].gender = (persons[i].sex === 'F') ? "Nữ" : "Nam";
+                persons[i].totalDistanceText = format()(persons[i].distance.toFixed(2), { noSeparator: false });
+
+                persons[i].url = util.format("https://www.strava.com/athletes/%d", persons[i].id);
             }
-            else
-            {
-                console.log("- fetching info of team #" + this.team.id + " FAILED");
-                console.log(err);
-            }
-        }.bind({ team : teams[i] }));
 
-        //console.log("- fetching members for team #" + teams[i].id);
-        var params = { id: teams[i].id, per_page: 200 };
-        strava.clubs.listMembers(params, function(err, members, limits) {
-            
-            if(!err)
-            {
-                //console.log("- fetching members for team #" + this.team.id + " SUCCESS");
-                //console.log("- team name = " + this.team.name);
-                //console.log("- total members = " + members.length);
+            res.render('index', { title: 'VNG Level Up 12+1 - UPRACE', data: persons });
+        }
+    });
+});
 
-                //console.log("---- fetching activities for team #" + this.team.id);
+/* GET home page. */
+router.get('/a', function(req, res, next) {
 
-                uprace.updateMembers(this.team, members);
+    var data = {
+        dates: [],
+        teams: []
+    };
 
-                /*
-                var params = { id: this.team.id, before: uprace.config.endDate, per_page: 200 };
-                strava.clubs.listActivities(params, function(err, activities, limits){
-                
-                    if (!err)
+    for(var i=0;i<12;i++)
+        data.dates.push(i);
+
+    async.each(data.dates,
+        function(item, callback){
+
+            fs.readFile(util.format("data/a_day_%d.json", item + 1), function(err, content){
+
+                if (err){
+                    callback();
+                }
+                else{
+                    var teams = JSON.parse(content);
+                    for(var i=0;i<teams.length;i++)
                     {
-                        //console.log("---- fetching activities for team #" + this.team.id + " SUCCESS");
-                        //console.log("---- total activities : " + activities.length);
-
-                        uprace.updateActivities(this.team, activities);
-                        uprace.rankingTeam(this.team);
-
-                        // check if this is the last callback
-
-                        uprace.waitCount--;
-                        if (uprace.waitCount <= 0)
+                        var team = null;
+                        for(var j=0;j<data.teams.length;j++)
                         {
-                            uprace.rankingEvent(teams);
-                            res.render('index', { title: 'VNG Level Up 12+1 - UPRACE', data: teams });
+                            if (data.teams[j].id === teams[i].id){
+                                team = data.teams[j];
+                                break;
+                            }
+                        }
+                        
+                        if (team == null || team === undefined){
+
+                            team = {
+                                id: teams[i].id,
+                                name: teams[i].name
+                            };
+
+                            team.totals = [];
+                            team.members = [];
+
+                            team.picture = "";
+                            team.profile = util.format("https://www.strava.com/clubs/%d", team.id);
+
+                            for(var j=0;j<data.dates.length;j++){
+                                team.totals.push({ number: 0, text: "--" });
+                            }
+
+                            for(var j=0;j<teams[i].athletes.length;j++){
+
+                                if (teams[i].athletes[j].isspy)
+                                    continue;
+
+                                var totals = [];
+                                for(var k=0;k<data.dates.length;k++)
+                                    totals.push({ number: 0, text: "--" });
+
+                                var member = {
+                                    id: teams[i].athletes[j].id,
+                                    name: teams[i].athletes[j].name,
+                                    gender: ((teams[i].athletes[j].sex == "F") ? "Nữ" : "Nam"),
+                                    picture: teams[i].athletes[j].profile_medium,
+                                    profile: util.format("https://www.strava.com/athletes/%d", teams[i].athletes[j].id),
+                                    distances: totals
+                                };
+                                
+                                team.members.push(member);
+                            }
+
+                            data.teams.push(team);
+                        }
+
+                        if (team){
+
+                            team.totals[item] = {
+                                number: teams[i].totalDistance / 1000,
+                                text: format()((teams[i].totalDistance / 1000).toFixed(2), { noSeparator: false })
+                            };
+                            
+                            for(var j=0;j<team.members.length;j++){
+                                for(var k=0;k<teams[i].athletes.length;k++){
+
+                                    if (team.members[j].id == teams[i].athletes[k].id){
+                                        team.members[j].distances[item] = {
+                                            number: teams[i].athletes[k].distance,
+                                            text: format()(teams[i].athletes[k].distance.toFixed(2), { noSeparator: false })
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
-                    else
-                    {
-                        console.log("---- fetching activities for team #" + this.team.id + " FAILED");
-                        console.log(err);
 
-                        res.render('index', { title: 'VNG Level Up 12+1 - UPRACE', data: [] });
+                    callback();
+                }
+            });
+        },
+        function(err){
+
+            data.teams.forEach(function(item){
+
+                for(var i=0;i<item.members.length;i++){
+
+                    var total = 0;
+                    for(var j=0;j<item.members[i].distances.length;j++)
+                        total += item.members[i].distances[j].number;
+
+                    item.members[i].rank = i+1;
+                    item.members[i].totalDistance = {
+                        number: total,
+                        text: format()(total.toFixed(2), { noSeparator: false })
                     }
-                }.bind({ team: this.team }));
-                */
+                }
 
-                uprace.getTeamActivities(this.team, function(error, data, limits){
+                var total = 0;
+                for(var i=0;i<item.totals.length;i++)
+                    total += item.totals[i].number;
 
-                    if (!err)
+                item.totalDistance = {
+                    number: total,
+                    text: format()(total.toFixed(2), { noSeparator: false })
+                };
+            });
+
+            data.teams.sort(function(a, b){
+                return a.totalDistance < b.totalDistance;
+            })
+
+            for(var i=0;i<data.teams.length;i++){
+                data.teams[i].rank = i+1;
+                data.teams[i].isTop = i+1 <= 3;
+            }
+
+            res.render('index-team-a', { title: 'VNG Level Up 12+1 - UPRACE', data: data });
+        }
+    );
+});
+
+/* GET home page. */
+router.get('/b', function(req, res, next) {
+
+    var data = {
+        dates: [],
+        teams: []
+    };
+
+    for(var i=0;i<12;i++)
+        data.dates.push(i);
+
+    async.each(data.dates,
+        function(item, callback){
+
+            fs.readFile(util.format("data/b_day_%d.json", item + 1), function(err, content){
+
+                if (err){
+                    callback();
+                }
+                else{
+                    var teams = JSON.parse(content);
+                    for(var i=0;i<teams.length;i++)
                     {
-                        console.log("---- fetching activities for team #" + this.team.id + " SUCCESS");
-                        console.log("---- total activities = " + data.length);
-
-                        uprace.updateActivities(this.team, data);
-                        uprace.rankingTeam(this.team);
-
-                        uprace.waitCount--;
-                        if (uprace.waitCount <= 0)
+                        var team = null;
+                        for(var j=0;j<data.teams.length;j++)
                         {
-                            uprace.rankingEvent(teams);
-                            res.render('index', { title: 'VNG Level Up 12+1 - UPRACE', data: teams });
+                            if (data.teams[j].id === teams[i].id){
+                                team = data.teams[j];
+                                break;
+                            }
+                        }
+                        
+                        if (team == null || team === undefined){
+
+                            team = {
+                                id: teams[i].id,
+                                name: teams[i].name
+                            };
+
+                            team.totals = [];
+                            team.members = [];
+
+                            team.picture = "";
+                            team.profile = util.format("https://www.strava.com/clubs/%d", team.id);
+
+                            for(var j=0;j<data.dates.length;j++){
+                                team.totals.push({ number: 0, text: "--" });
+                            }
+
+                            for(var j=0;j<teams[i].athletes.length;j++){
+
+                                if (teams[i].athletes[j].isspy)
+                                    continue;
+
+                                var totals = [];
+                                for(var k=0;k<data.dates.length;k++)
+                                    totals.push({ number: 0, text: "--" });
+
+                                var member = {
+                                    id: teams[i].athletes[j].id,
+                                    name: teams[i].athletes[j].name,
+                                    gender: ((teams[i].athletes[j].sex == "F") ? "Nữ" : "Nam"),
+                                    picture: teams[i].athletes[j].profile_medium,
+                                    profile: util.format("https://www.strava.com/athletes/%d", teams[i].athletes[j].id),
+                                    distances: totals
+                                };
+
+                                team.members.push(member);
+                            }
+
+                            data.teams.push(team);
+                        }
+
+                        if (team){
+
+                            team.totals[item] = {
+                                number: teams[i].totalDistance / 1000,
+                                text: format()((teams[i].totalDistance / 1000).toFixed(2), { noSeparator: false })
+                            };
+                            
+                            for(var j=0;j<team.members.length;j++){
+                                for(var k=0;k<teams[i].athletes.length;k++){
+
+                                    if (team.members[j].id == teams[i].athletes[k].id){
+                                        team.members[j].distances[item] = {
+                                            number: teams[i].athletes[k].distance,
+                                            text: format()(teams[i].athletes[k].distance.toFixed(2), { noSeparator: false })
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
-                    else
-                    {
-                        console.log("---- fetching activities for team #" + this.team.id + " FAILED");
-                        console.log(err);
 
-                        res.render('index', { title: 'VNG Level Up 12+1 - UPRACE', data: [] });
+                    callback();
+                }
+            });
+        },
+        function(err){
+
+            data.teams.forEach(function(item){
+
+                for(var i=0;i<item.members.length;i++){
+
+                    var total = 0;
+                    for(var j=0;j<item.members[i].distances.length;j++)
+                        total += item.members[i].distances[j].number;
+
+                    item.members[i].rank = i+1;
+                    item.members[i].totalDistance = {
+                        number: total,
+                        text: format()(total.toFixed(2), { noSeparator: false })
                     }
-                }.bind({ team: this.team }));
-            }
-            else
-            {
-                console.log("- fetching members for team #" + this.team.id + " FAILED");
-                console.log(err);
+                }
 
-                res.render('index', { title: 'VNG Level Up 12+1 - UPRACE', data: [] });
+                var total = 0;
+                for(var i=0;i<item.totals.length;i++)
+                    total += item.totals[i].number;
+
+                item.totalDistance = {
+                    number: total,
+                    text: format()(total.toFixed(2), { noSeparator: false })
+                };
+            });
+
+            data.teams.sort(function(a, b){
+                return a.totalDistance < b.totalDistance;
+            })
+
+            for(var i=0;i<data.teams.length;i++){
+                data.teams[i].rank = i+1;
+                data.teams[i].isTop = i+1 <= 3;
             }
-        }.bind({ team : teams[i] }));
-    }
+
+            res.render('index-team-b', { title: 'VNG Level Up 12+1 - UPRACE', data: data });
+        }
+    );
 });
 
 /* GET about. */
